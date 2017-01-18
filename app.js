4210497ef9a276467beac97ad423224ffcded7ae
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-01-18 13:25:43 -0600

    added controllers, and config, added ng-view

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('git sucks')
+console.log("ndewdlk")
+
+// create angular module
+
+var app = angular.module('highwayApp', ['ngRoute'])
+// add ngRoute info app
+
+app.config( function ($routeProvider, $locationProvider) {
+  $locationProvider.hashPrefix('')
+  $routeProvider
+    .when('/', {
+      controller: '66Ctrl',
+      templateURL: 'partials/66.html'
+    })
+    .when('/blue', {
+      controller: 'BlueCtrl',
+      templateURL: 'partials/blue.html'
+    })
+    .otherwise({redirectTo:'/'})
+})
+
+app.controller('66Ctrl', function($scope) {
+  $scope.highway = "route 66"
+  console.log('66')
+})
+
+app.controller('BlueCtrl', function($scope) {
+  $scope.highway = "Blue Ridge"
+  console.log('blue')
+})
